fd2d87ac11aa4db551afd5a198ebc9406feb4583
Jose Maria Bernad <josemari.bernad@gmail.com>
2018-11-30 10:49:51 +0000

    Transform images to black over white

--- data_augmentations.py
+++ data_augmentations.py
@@ -3,16 +3,19 @@ import cv2
 #import matplotlib.pyplot as plt
 from pathlib import Path
 
-def invert(image_path):
+def invert(image_path, replace = False):
     try:
-        image_path_inverted = image_path.with_name('invert' + image_path.name)
+        if replace:
+            image_path_inverted = image_path
+        else:
+            image_path_inverted = image_path.with_name('invert' + image_path.name)
         img = cv2.imread(str(image_path))
         img = cv2.bitwise_not(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         #plt.imshow(img)
         cv2.imwrite(str(image_path_inverted), img)
     except:
-        print("Error while augmenting " + str(image_path))
+        print("Error while inverting " + str(image_path))
     
 def augment(path, augmentation, is_recursive = True):
     for file in path.iterdir():
@@ -21,3 +24,11 @@ def augment(path, augmentation, is_recursive = True):
                 augment(file, augmentation)
         else:
             augmentation(file)
+
+def transform(path, transformation, is_recursive = True):
+    for file in path.iterdir():
+        if file.is_dir():
+            if is_recursive:
+                transform(file, transformation)
+        else:
+            transformation(file, True)
